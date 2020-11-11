3b04388d1ed540a39fc7c318ccc986c20102bd7c
Avelon Pang <avelonpang@gmail.com>
2020-11-11 00:38:57 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -27,7 +27,25 @@ def list(my_songs)
 	end
 	
 	def play(my_songs)
-	 
+	puts "Please enter a song name:"
+  user_response = gets.chomp
+  my_songs.each { |song, location|
+    if user_response == song
+      system 'open ' + location
+    end
+      }
+  if user_response == "list"
+    list(my_songs)
+    play(my_songs)
+  else
+    puts "Invalid input, please try again:"
+    play(my_songs)
+  end
+end
+
+
+def exit_jukebox	
+  
 
 # def help
 #   #puts "Please enter a command:"
