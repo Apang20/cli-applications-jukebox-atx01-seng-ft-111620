d8bf4a864dbd1ae636c9d74611e674f19a36ff57
Avelon Pang <avelonpang@gmail.com>
2020-11-11 00:49:52 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -30,7 +30,7 @@ end
 def play(songs)
   puts "Please enter a song name or number:"
   choice = gets.chomp
-  #valid = false
+  valid = false
   songs.each_with_index do |song, index|
     #binding.pry
     if (choice.to_i - 1 == index)
