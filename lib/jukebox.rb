c7cf13a8c0033d209ba2b8e8be80434f10124367
Avelon Pang <avelonpang@gmail.com>
2020-11-11 00:34:42 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -12,18 +12,24 @@ songs = [
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
 
-def help
-  #puts "Please enter a command:"
-  puts "I accept the following commands:"
-  puts "- help : displays this help message"
-  puts "- list : displays a list of songs you can play"
-  puts "- play : lets you choose a song to play"
-  puts "- exit : exits this program"
-end
 
-def list(songs)
-  songs.each_with_index{|song, index| puts "#{index + 1}. #{song}"}
-end
+
+
+
+
+
+# def help
+#   #puts "Please enter a command:"
+#   puts "I accept the following commands:"
+#   puts "- help : displays this help message"
+#   puts "- list : displays a list of songs you can play"
+#   puts "- play : lets you choose a song to play"
+#   puts "- exit : exits this program"
+# end
+
+# def list(songs)
+#   songs.each_with_index{|song, index| puts "#{index + 1}. #{song}"}
+# end
 
 
 # def play(songs)
